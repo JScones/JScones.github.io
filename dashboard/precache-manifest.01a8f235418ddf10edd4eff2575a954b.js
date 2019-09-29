d6c034a70cbb75a5458f07744ceadd3e49236596
Josh Bernasconi <jbe113@uclive.ac.nz>
2019-09-29 13:02:14 +1300

    Add dashboard v1

---
+++ dashboard/precache-manifest.01a8f235418ddf10edd4eff2575a954b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126666e067dc8a5a74dbcb4e112bf5bf",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "ebce48906a6832ebd2e3",
+    "url": "/dashboard/static/css/main.a655d47e.chunk.css"
+  },
+  {
+    "revision": "ae6e95a0d93e9f22382e",
+    "url": "/dashboard/static/js/2.c173b193.chunk.js"
+  },
+  {
+    "revision": "ebce48906a6832ebd2e3",
+    "url": "/dashboard/static/js/main.3ed3093b.chunk.js"
+  },
+  {
+    "revision": "05dc75466a13b1c2b24d",
+    "url": "/dashboard/static/js/runtime-main.a1705bb5.js"
+  }
+]);
